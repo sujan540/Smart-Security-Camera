719ee7af1509e24bd242c64c85df97a0ba396aef
Sujan <thesujan540@gmail.com>
2019-12-01 18:12:39 -0600

    update

--- camera.py
+++ camera.py
@@ -6,44 +6,35 @@ import numpy as np
 
 class VideoCamera(object):
     def __init__(self, flip = False):
-        self.vs = PiVideoStream().start()
+        self.vs = None
         self.flip = flip
-        time.sleep(2.0)
 
     def __del__(self):
-        self.vs.stop()
+        if self.vs is not None:
+            self.vs.stop()
 
+    def start(self, flip = False):
+        if self.vs is None:
+            self.vs = PiVideoStream().start()
+            self.flip = flip
+            time.sleep(2.0)
+        
+    def stop(self):
+        if self.vs is not None:
+            self.vs.stop()
+            self.vs = None
+        
     def flip_if_needed(self, frame):
         if self.flip:
             return np.flip(frame, 0)
         return frame
 
     def get_frame(self):
+        if self.vs is None:
+            return
         frame = self.flip_if_needed(self.vs.read())
         ret, jpeg = cv2.imencode('.jpg', frame)
         return jpeg.tobytes()
 
-    def get_object(self, classifier):
-        found_objects = False
-        frame = self.flip_if_needed(self.vs.read()).copy() 
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-        objects = classifier.detectMultiScale(
-            gray,
-            scaleFactor=1.1,
-            minNeighbors=5,
-            minSize=(30, 30),
-            flags=cv2.CASCADE_SCALE_IMAGE
-        )
-
-        if len(objects) > 0:
-            found_objects = True
-
-        # Draw a rectangle around the objects
-        for (x, y, w, h) in objects:
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-
-        ret, jpeg = cv2.imencode('.jpg', frame)
-        return (jpeg.tobytes(), found_objects)
 
 
